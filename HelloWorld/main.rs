edea2ba0340a6f9e4c411e99cd047349b7c04043
Wonjun Jang <wonwonjun@gmail.com>
2022-01-22 23:54:20 +0900

    Practice my first HelloWorld example with Rust

---
+++ HelloWorld/main.rs
@@ -0,0 +1,21 @@
+/*  
+
+  Rust files always end with the .rs extension. 
+  If you’re using more than one word in your filename, 
+  use an underscore to separate them. 
+  For example, use hello_world.rs rather than helloworld.rs.
+
+*/
+
+// this is how you would define a function in Rust
+
+/*
+
+    The main function is special: 
+    it is always the first code that runs 
+    in every executable Rust program.
+
+*/
+fn main() {
+  println!("Hello, world!");
+}
